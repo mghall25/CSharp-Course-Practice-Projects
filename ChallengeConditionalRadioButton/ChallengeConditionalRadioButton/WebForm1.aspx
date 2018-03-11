03c6174e2c4c549b30c1d6004855fbce28e8cd49
MGH <miriamhall.mgh@gmail.com>
2018-03-10 19:14:03 -0800

    Add ChallengeConditionalRadioButton
    if..then, add an image based on selection

---
+++ ChallengeConditionalRadioButton/ChallengeConditionalRadioButton/WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ChallengeConditionalRadioButton.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Your Note-Taking Preference</h1>
+            <asp:RadioButton ID="pencilRadio" runat="server" GroupName="noteGroup" OnCheckedChanged="RadioButton1_CheckedChanged" Text="Pencil" />
+            <br />
+            <asp:RadioButton ID="penRadio" runat="server" GroupName="noteGroup" Text="Pen" />
+            <br />
+            <asp:RadioButton ID="phoneRadio" runat="server" GroupName="noteGroup" Text="Phone" />
+            <br />
+            <asp:RadioButton ID="tabletRadio" runat="server" GroupName="noteGroup" Text="Tablet" />
+            <br />
+            <br />
+            <asp:Button ID="OkBtn" runat="server" OnClick="OkBtn_Click" Text="OK" />
+            <br />
+            <br />
+            <asp:Label ID="msgLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Image ID="selectedImg" runat="server" Width="400px" />
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ ChallengeConditionalRadioButton/ChallengeConditionalRadioButton/WebForm1.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void OkBtn_Click(object sender, EventArgs e)
+        {
+
+            if (pencilRadio.Checked)
+            {
+                msgLabel.Text = "";
+                selectedImg.ImageUrl = "../pencil.png";
+                        
+            }
+            else if (penRadio.Checked)
+            {
+                msgLabel.Text = "";
+                selectedImg.ImageUrl = "../pen.png";
+            }
+            else if (phoneRadio.Checked)
+            {
+                msgLabel.Text = "";
+                selectedImg.ImageUrl = "../phone.png";
+            }
+            else if (tabletRadio.Checked)
+            {
+                msgLabel.Text = "";
+                selectedImg.ImageUrl = "../tablet.png";
+            }
+            else
+            {
+                msgLabel.Text = ("Please select an option.");
+            }
+        }
+    }
+}
